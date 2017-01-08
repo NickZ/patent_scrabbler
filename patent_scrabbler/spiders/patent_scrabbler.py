ef872e11b750d6d8a0754b5cda984e0d4af0af56
Nick Zatkovich <NZatkovich@gmail.com>
2017-01-07 18:07:27 -0800

    bugfixes

--- patent_scrabbler/spiders/patent_scrabbler.py
+++ patent_scrabbler/spiders/patent_scrabbler.py
@@ -8,21 +8,23 @@ class PatentScrabblerSpider(scrapy.Spider):
     google_patents_url = "https://patents.google.com/patent/"
     start_urls = []
     def __init__(self, inputfile='', *args, **kwargs):
-        super(PatentScrabblerSpider, self, *args, **kwargs)
+        super(PatentScrabblerSpider, self).__init__(*args, **kwargs)
         if inputfile == '':
             raise Exception("Need an input file!")
         self.inputfile = inputfile
         for line in open(inputfile):
             line = line.strip()
-            if line.find("US") == -1:
+            if line.find("US") == -1 and line.find("WO") == -1:
                 line = "US" + line
             line = self.google_patents_url + line
             self.start_urls.append(line)
-    
+
 
     def parse(self, response):
         item = PatentScrabblerItem()
-        item['patent_number'] = response.xpath('//meta[@name="citation_patent_number"]/@content').extract().pop().strip()
+        #Use number without codes instead of the citation patent number.
+        #item['patent_number'] = response.xpath('//meta[@name="citation_patent_number"]/@content').extract().pop().strip()
+        item['patent_number'] = response.xpath('//meta[@itemprop="numberWithoutCodes"]/@content').extract().pop().strip()
         item['title'] = response.xpath('//meta[@name="DC.title"]/@content').extract().pop().strip()
         item['priority_date'] = response.xpath('//time[@itemprop="priorityDate"]/text()').extract().pop().strip()
         item['filing_date'] = response.xpath('//time[@itemprop="filingDate"]/text()').extract().pop().strip()
